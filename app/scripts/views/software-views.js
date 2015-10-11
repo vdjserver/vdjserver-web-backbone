c89ebff8b7f55b1108e16ba997e23ab7d5bdc153
Walter Scarborough <wscarbor@tacc.utexas.edu>
2015-10-11 14:07:45 -0500

    Fixed deprecated vdjml route workaround

--- app/scripts/views/software-views.js
+++ app/scripts/views/software-views.js
@@ -8,14 +8,6 @@ define([
 
     Software.Index = Backbone.View.extend({
         template: 'software/index',
-        events: {
-            'click #vdjml-link': 'openVdjmlLink',
-        },
-        openVdjmlLink: function(e) {
-            e.preventDefault();
-
-            window.open('https://vdjserver.org/vdjml');
-        },
     });
 
     App.Views.Software = Software;
