9d29d36c2b6e8018112f293efc151494527f1d26
Scott Christley <Scott.Christley@UTSouthwestern.edu>
2016-06-14 17:48:34 -0500

    bring in full web-app

--- component/test/spec/vendor/agave-tests.js
+++ component/test/spec/vendor/agave-tests.js
@@ -1,6 +1,6 @@
 /*global describe, it */
 
-define(['app', 'backbone-agave'], function() {
+define(['app', 'backbone-agave'], function(App) {
 
     'use strict';
 
@@ -14,6 +14,43 @@ define(['app', 'backbone-agave'], function() {
         it('Auth token model should exist', function() {
             var authTokenModel = new Backbone.Agave.Auth.Token();
             should.exist(authTokenModel);
+        });
+
+        it('Should perform login', function() {
+
+            should.exist(App);
+            App.init();
+
+            should.exist(App.Agave);
+
+            var model = App.Agave.token();
+
+            console.log(model);
+            App.Agave.destroyToken();
+
+            // TODO: need to pull this in from config/env
+            var formData = {
+                username: '',
+                password: ''
+            };
+
+            model.save(formData, {password: formData.password})
+                .done(function() {
+
+                    //assert.strictEqual(jqXHR.statusCode, 200);
+
+                    var projectUuid = '9057880830218530330-242ac11b-0001-012';
+                    var projectFiles = new Backbone.Agave.Collection.Files.Metadata({projectUuid: projectUuid});
+                    projectFiles.fetch()
+                        .then(function() {
+                            console.log('Got projectFiles');
+                            console.log(projectFiles);
+                        })
+                        .fail(function(error) {
+                            console.log(error);
+                        });
+
+                });
         });
 
     });
