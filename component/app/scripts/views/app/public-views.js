7037fc5e44a8f785fb766c8391e7092886ac0058
Scott Christley <scott.christley@utsouthwestern.edu>
2020-04-18 10:43:24 -0500

    copyright, reformat

--- component/app/scripts/views/app/public-views.js
+++ component/app/scripts/views/app/public-views.js
@@ -1,3 +1,30 @@
+//
+// public-views.js
+// Home login page
+//
+// VDJServer Analysis Portal
+// Web Interface
+// https://vdjserver.org
+//
+// Copyright (C) 2020 The University of Texas Southwestern Medical Center
+//
+// Author: Scott Christley <scott.christley@utsouthwestern.edu>
+// Author: Olivia Dorsey <olivia.dorsey@utsouthwestern.edu>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+
 import Marionette from 'backbone.marionette';
 import template from '../../../templates/app/home.html';
 import login_template from '../../../templates/app/login.html';
@@ -14,23 +41,23 @@ var ModalRegion = Marionette.Region.extend({
 
 // the bootstrap modal view
 var ModalMessageConfirm = Marionette.View.extend({
-  template: Handlebars.compile(modal_template),
-  region: '#modal'
+    template: Handlebars.compile(modal_template),
+    region: '#modal'
 });
 
 var LoginView = Marionette.View.extend({
-  template: Handlebars.compile(login_template)
+    template: Handlebars.compile(login_template)
 });
 
 // login window with modal region
 export default Marionette.View.extend({
-  template: Handlebars.compile(template),
-  regions: {
-    homeRegion: '#home',
-    modalRegion: {
-      el: '#modal',
-      regionClass: ModalRegion
-    }
+    template: Handlebars.compile(template),
+    regions: {
+        homeRegion: '#home',
+        modalRegion: {
+            el: '#modal',
+            regionClass: ModalRegion
+        }
   },
 
   initialize: function(parameters) {
@@ -45,7 +72,6 @@ export default Marionette.View.extend({
       'click #home-login': 'login',
       'shown.bs.modal': 'onShownModal',
       'hidden.bs.modal': 'onHiddenModal',
-      //'click #create-project': 'create'
   },
 
   onShownModal() {
@@ -57,39 +83,41 @@ export default Marionette.View.extend({
     // go perform the login
     console.log(this.loginState);
     if (this.loginState == 'login') {
-      var formData = {
+        var formData = {
           username: $('#username').val(),
           password: $('#password').val()
-      };
+        };
 
-      App.Agave.token().save(formData, {password: formData.password})
-        .done(function() {
-            that.loginState = 'pass';
-            $('#modal-message').modal('hide');
-            console.log("login pass");
-        })
-        .fail(function(error) {
-            // login failed so change state, hide the current modal
-            console.log(error);
-            that.loginState = 'fail';
-            $('#modal-message').modal('hide');
+        // login
+        App.Agave.token().save(formData, {password: formData.password})
+            .done(function() {
+                that.loginState = 'pass';
+                // wait until modal is hidden before routing
+                $('#modal-message').modal('hide');
+                console.log("login pass");
+            })
+            .fail(function(error) {
+                // login failed so change state, hide the current modal
+                console.log(error);
+                that.loginState = 'fail';
+                $('#modal-message').modal('hide');
 
-            // clear token and form password
-            App.Agave.token().clear();
-            $('#password').val('');
+                // clear token and form password
+                App.Agave.token().clear();
+                $('#password').val('');
 
-            // prepare a new modal with the failure message
-            var message = new MessageModel({
-                'header': 'Logging in',
-                'body':   '<p>Login failed...</p>'
+                // prepare a new modal with the failure message
+                var message = new MessageModel({
+                    'header': 'Logging in',
+                    'body':   '<p>Login failed...</p>'
+                });
+                var view = new ModalMessageConfirm({model: message});
+                that.showChildView('modalRegion', view);
+                $('#confirmation-button').removeClass('hidden');
+                $('#modal-message').modal('show');
+
+                console.log("login fail");
             });
-            var view = new ModalMessageConfirm({model: message});
-            that.showChildView('modalRegion', view);
-            $('#confirmation-button').removeClass('hidden');
-            $('#modal-message').modal('show');
-
-            console.log("login fail");
-        });
     } else if (that.loginState == 'fail') {
       // if login failed, then we are showing the fail modal
     }
@@ -97,7 +125,6 @@ export default Marionette.View.extend({
 
   onHiddenModal() {
     console.log('login: Hide the modal');
-    //this.getRegion().empty();
     if (this.loginState == 'pass') {
         // login passed so route to the project list view
         App.router.navigate('/project', {trigger:true});
@@ -127,130 +154,3 @@ export default Marionette.View.extend({
 
   }
 });
-
-
-/*
-define([
-    'app',
-    'backbone.syphon',
-], function(App) {
-
-    'use strict';
-
-    var Public = {};
-
-    Public.Home = Backbone.View.extend({
-        template: 'public/home',
-        initialize: function() {
-
-            //this.model.destroy();
-            this.render;
-        },
-        serialize: function() {
-            return {
-                'username': this.model.get('username'),
-                'maintenance': EnvironmentConfig.agave.maintenance,
-                'maintenanceMessage': EnvironmentConfig.agave.maintenanceMessage
-            };
-        },
-        afterRender: function() {
-            this.setupModalView();
-        },
-        setupModalView: function() {
-
-            var message = new App.Models.MessageModel({
-                'header': 'Logging in',
-                'body':   '<p>Please wait while we authenticate you...</p>'
-            });
-
-            var modal = new App.Views.Util.ModalMessageConfirm({
-                model: message
-            });
-
-            $('<div id="modal-view">').appendTo(this.el);
-
-            this.setView('#modal-view', modal);
-            modal.render();
-
-        },
-        events: {
-            'click #home-login': 'login'
-        },
-        login: function(e) {
-
-            e.preventDefault();
-
-            this.$el.find('.alert-danger').fadeOut(function() {
-                this.remove();
-            });
-
-            var formData = {
-                username: $('#username').val(),
-                password: $('#password').val()
-            };
-
-            //var formData = Backbone.Syphon.serialize(this);
-
-            //this.setupModalView();
-
-            var that = this;
-
-            $('#modal-message')
-                .modal('show')
-                .on('shown.bs.modal', function() {
-
-                    that.model
-                        .save(formData, {password: formData.password})
-                        .done(function() {
-                            $('#modal-message')
-                                .modal('hide')
-                                .on('hidden.bs.modal', function() {
-
-                                    App.router.navigate('/project', {
-                                        trigger: true
-                                    });
-
-                                });
-                        })
-                        .fail(function(error) {
-
-                            var telemetry = new Backbone.Agave.Model.Telemetry();
-                            telemetry.setError(error);
-                            telemetry.set('method', 'App.Agave.token().save()');
-                            telemetry.set('view', 'Public.Home');
-                            telemetry.save();
-
-                            if (error && error['status']) {
-
-                                var httpCode = error['status'];
-
-                                if (httpCode === 403) {
-                                    App.router.navigate('/account/pending', {
-                                        trigger: true
-                                    });
-
-                                    return;
-                                }
-                            }
-
-                            App.Agave.token().clear();
-                            $('#confirmation-button').removeClass('hidden');
-                            $('.modal-body').html('');
-
-                            $('.modal-body').prepend(
-                                $('<div class="alert alert-danger">')
-                                    .text('Authentication failed. Please check your username and password')
-                                    .fadeIn()
-                            );
-
-                            $('#password').val('');
-                        });
-                });
-            return false;
-        },
-    });
-
-    App.Views.Public = Public;
-    return Public;
-});
-*/
