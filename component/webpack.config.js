48c4ae2d86bd29dc195172bcf6d1479772c84136
Scott Christley <scott.christley@utsouthwestern.edu>
2020-04-24 14:15:29 -0500

    copyright/license

--- component/webpack.config.js
+++ component/webpack.config.js
@@ -1,10 +1,41 @@
+//
+// webpack.config.js
+// Webpack configuration
+//
+// VDJServer Analysis Portal
+// Web Interface
+// https://vdjserver.org
+//
+// Copyright (C) 2020 The University of Texas Southwestern Medical Center
+//
+// Author: Olivia Dorsey <olivia.dorsey@utsouthwestern.edu>
+// Author: Scott Christley <scott.christley@utsouthwestern.edu>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+
 const path = require('path');
 const webpack = require('webpack');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = function(env, argv) {
+//
+// We have four configurations
+// development mode or production mode
+// main app or test app
+//
 
-  console.log(env);
+module.exports = function(env, argv) {
 
     // default settings
     var settings = {
