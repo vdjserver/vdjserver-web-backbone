e509099988ed6d8ff81fe6fa2587d1a85dc8dfe4
Scott Christley <scott.christley@utsouthwestern.edu>
2020-04-24 14:02:07 -0500

    copyright/license

--- component/app/scripts/entry.js
+++ component/app/scripts/entry.js
@@ -1,3 +1,30 @@
+//
+// entry.js
+// Webpack entry point for main application
+//
+// VDJServer Analysis Portal
+// Web Interface
+// https://vdjserver.org
+//
+// Copyright (C) 2020 The University of Texas Southwestern Medical Center
+//
+// Author: Scott Christley <scott.christley@utsouthwestern.edu>
+// Author: Olivia Dorsey <olivia.dorsey@utsouthwestern.edu>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+
 // Custom jQuery
 import './_custom.js';
 
@@ -8,9 +35,7 @@ import '../styles/main.scss'
 // Images
 import logo from '../images/logo.png';
 
-// Main JS
-//import 'top-level';
-
+// main application
 import { Agave } from 'backbone-agave';
 import Application from 'app';
 
@@ -18,8 +43,6 @@ import Application from 'app';
 import bootstrap from 'bootstrap';
 
 document.addEventListener('DOMContentLoaded', () => {
-  // the Agave object, attached to the global Backbone object
-  //Backbone.Agave = Agave;
   // the global application object
   global.App = new Application();
   // start the application
