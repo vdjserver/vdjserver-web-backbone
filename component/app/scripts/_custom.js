daeee7b9e4b82c9228334f87af6ff9d775bbf6d4
Olivia Dorsey <Olivia.Dorsey@UTSouthwestern.edu>
2020-05-27 14:22:31 -0500

    Green bar steps change based on step in Create Repertoire process

--- component/app/scripts/_custom.js
+++ component/app/scripts/_custom.js
@@ -2,14 +2,55 @@ $(document).ready(function() {
     console.log("jquery works");
 
 // Summary Card Active States
-      $(document).on('click', '.card-link', function() {
-          console.log("card clicked");
-          $('.card-link').removeClass("card-active");
-          $(this).addClass("card-active");
-      });
+    $(document).on("click", ".card-link", function() {
+        $(".card-link").removeClass("card-active");
+        $(this).addClass("card-active");
+    });
+
 // Click Edit Repertoire button
-     $(document).on('click', '#edit-repertoire', function() {
-        console.log("clicked from jquery");
-        $(this).addClass("hide");
+     $(document).on("click", "#edit-repertoire", function() {
+        $(this).addClass("no-display");
+    });
+
+// Click Save Repertoire button
+    $(document).on("click", "#save-repertoire", function() {
+       $(this).addClass("no-display");
+    });
+
+// Steps for Create a Repertoire
+    // Add Repertoire
+    $(document).on("click", "#create-rep", function() {
+        $(".step").removeClass("selected-step");
+        $("#repertoire-step").addClass("selected-step");
+    });
+
+    // Add Subject
+    $(document).on("click", "#add-subject", function() {
+        $(".step").removeClass("selected-step");
+        $("#subject-step").addClass("selected-step");
+    });
+
+    // Add Diagnosis
+    $(document).on("click", "#add-diagnosis", function() {
+        $(".step").removeClass("selected-step");
+        $("#diagnosis-step").addClass("selected-step");
+    });
+
+    // Add Sample
+    $(document).on("click", "#add-sample", function() {
+        $(".step").removeClass("selected-step");
+        $("#sample-step").addClass("selected-step");
+    });
+
+    // Add Cell Processing
+    $(document).on("click", "#add-cell", function() {
+        $(".step").removeClass("selected-step");
+        $("#cell-step").addClass("selected-step");
+    });
+
+    // Add Nucleic Acid Processing
+    $(document).on("click", "#add-nucleic", function() {
+        $(".step").removeClass("selected-step");
+        $("#nucleic-step").addClass("selected-step");
     });
 });
