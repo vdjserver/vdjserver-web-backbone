1e69a6dfe21b772a8071711bdf98e2fb033c3a27
Scott Christley <scott.christley@utsouthwestern.edu>
2020-06-22 13:30:23 -0500

    merge public/private navigation bar

--- component/app/scripts/views/app/navbar-controller.js
+++ component/app/scripts/views/app/navbar-controller.js
@@ -1,17 +1,59 @@
+//
+// navbar-controller.js
+// Manages the navigation bar for public/private
+//
+// VDJServer Analysis Portal
+// Web Interface
+// https://vdjserver.org
+//
+// Copyright (C) 2020 The University of Texas Southwestern Medical Center
+//
+// Author: Scott Christley <scott.christley@utsouthwestern.edu>
+// Author: Olivia Dorsey <olivia.dorsey@utsouthwestern.edu>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+
 import Marionette from 'backbone.marionette';
 import Handlebars from 'handlebars';
 
-import public_template from 'Templates/app/navbar-public.html';
-import private_template from 'Templates/app/navbar-private.html';
+// We use a single navigation bar template that is customized by handlebars
+// 1. public page, not logged in
+// 2. public page, logged in
+// 3. private page, logged in
+import navbar_template from 'Templates/app/navigation-bar.html';
 
-// This is the navigation bar displayed to the public
-var PublicNavBar = Marionette.View.extend({
-  template: Handlebars.compile(public_template)
-});
+// This is the navigation bar
+var NavigationBarView = Marionette.View.extend({
+    template: Handlebars.compile(navbar_template),
 
-// This is the navigation bar displayed after the user logs in
-var PrivateNavBar = Marionette.View.extend({
-  template: Handlebars.compile(private_template)
+    initialize: function(parameters) {
+        this.public_bar = true;
+        this.active_token = App.Agave.token().isActive();
+
+        if (parameters) {
+            if (parameters.public_bar != undefined)
+                this.public_bar = parameters.public_bar;
+        }
+    },
+
+    templateContext() {
+        return {
+            public_bar: this.public_bar,
+            active_token: this.active_token
+        };
+    },
 });
 
 // Navigation bar controller
@@ -35,11 +77,11 @@ export default Marionette.View.extend({
     },
 
     showPublicNavigation() {
-        this.showChildView('navigationRegion', new PublicNavBar());
+        this.showChildView('navigationRegion', new NavigationBarView({public_bar: true}));
     },
 
     showPrivateNavigation() {
-        this.showChildView('navigationRegion', new PrivateNavBar());
+        this.showChildView('navigationRegion', new NavigationBarView({public_bar: false}));
     },
 
     logout(e) {
