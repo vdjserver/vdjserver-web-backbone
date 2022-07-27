27173207cc92d2aa79c4f70089410e3c8afce95b
Scott Christley <scott.christley@utsouthwestern.edu>
2022-07-27 15:18:35 -0500

    update js

--- component/app/scripts/models/agave-password-change.js
+++ component/app/scripts/models/agave-password-change.js
@@ -1,104 +1,119 @@
-define(
-    [
-        'backbone',
-    ],
-function(
-    Backbone
-) {
 
-    'use strict';
+'use strict';
 
-    var PasswordChange = {};
+//
+// agave-password-change.js
+// Account change password model
+//
+// VDJServer Analysis Portal
+// Web Interface
+// https://vdjserver.org
+//
+// Copyright (C) 2022 The University of Texas Southwestern Medical Center
+//
+// Author: Scott Christley <scott.christley@utsouthwestern.edu>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
 
-    PasswordChange = Backbone.Agave.Model.extend({
-        defaults: {
-            username:  '',
-            password: '',   // old password
-            newPassword: '',
-            passwordCheck: ''
-        },
-        apiHost: EnvironmentConfig.vdjApi.hostname,
-        url: function() {
-            return '/user/change-password';
-        },
-        callSave: function() {
-            var jqxhr = $.ajax({
-                contentType: 'application/json',
-                data: JSON.stringify({
-                    newPassword: encodeURIComponent(this.get('newPassword')),
-                    password: encodeURIComponent(this.get('password')),
-                }),
-                headers: Backbone.Agave.basicAuthHeader(),
-                type: 'POST',
-                url: EnvironmentConfig.vdjApi.hostname + this.url(),
+import { Agave } from 'Scripts/backbone/backbone-agave';
+
+export var PasswordChange = Agave.Model.extend({
+    defaults: {
+        username:  '',
+        password: '',   // old password
+        newPassword: '',
+        passwordCheck: ''
+    },
+    apiHost: EnvironmentConfig.vdjApi.hostname,
+    url: function() {
+        return '/user/change-password';
+    },
+    callSave: function() {
+        var jqxhr = $.ajax({
+            contentType: 'application/json',
+            data: JSON.stringify({
+                newPassword: encodeURIComponent(this.get('newPassword')),
+                password: encodeURIComponent(this.get('password')),
+            }),
+            headers: Backbone.Agave.basicAuthHeader(),
+            type: 'POST',
+            url: EnvironmentConfig.vdjApi.hostname + this.url(),
+        });
+
+        return jqxhr;
+    },
+    validate: function(attributes) {
+
+        var errors = [];
+
+        // Missing attributes
+        if (!attributes.username) {
+            errors.push({
+                'message': 'Missing username.',
+                'type': 'username'
             });
-
-            return jqxhr;
-        },
-        validate: function(attributes) {
-
-            var errors = [];
-
-            // Missing attributes
-            if (!attributes.username) {
-                errors.push({
-                    'message': 'Missing username.',
-                    'type': 'username'
-                });
-            }
-
-            if (!attributes.password) {
-                errors.push({
-                    'message': 'Missing current password.',
-                    'type': 'password'
-                });
-            }
-
-            if (!attributes.newPassword) {
-                errors.push({
-                    'message': 'Missing new password.',
-                    'type': 'newPassword'
-                });
-            }
-
-            if (!attributes.passwordCheck) {
-                errors.push({
-                    'message': 'Missing new password verification.',
-                    'type': 'passwordCheck'
-                });
-            }
-
-            // Incorrect attributes
-            if (attributes.newPassword.length > 0 && attributes.newPassword.length < 5) {
-                errors.push({
-                    'message': 'Password is too short.',
-                    'type': 'newPassword'
-                });
-            }
-
-            if (attributes.username.length > 0 &&
-                attributes.newPassword.length > 0 &&
-                attributes.username.indexOf(attributes.newPassword) >= 0
-            ) {
-                errors.push({
-                    'message': 'Password can\'t be part of username.',
-                    'type': 'newPassword'
-                });
-            }
-
-            if (attributes.newPassword !== attributes.passwordCheck) {
-                errors.push({
-                    'message': 'Passwords do not match.',
-                    'type': 'passwordCheck'
-                });
-            }
-
-            if (errors.length) {
-                return errors;
-            }
         }
-    });
 
-    Backbone.Agave.Model.PasswordChange = PasswordChange;
-    return PasswordChange;
+        if (!attributes.password) {
+            errors.push({
+                'message': 'Missing current password.',
+                'type': 'password'
+            });
+        }
+
+        if (!attributes.newPassword) {
+            errors.push({
+                'message': 'Missing new password.',
+                'type': 'newPassword'
+            });
+        }
+
+        if (!attributes.passwordCheck) {
+            errors.push({
+                'message': 'Missing new password verification.',
+                'type': 'passwordCheck'
+            });
+        }
+
+        // Incorrect attributes
+        if (attributes.newPassword.length > 0 && attributes.newPassword.length < 5) {
+            errors.push({
+                'message': 'Password is too short.',
+                'type': 'newPassword'
+            });
+        }
+
+        if (attributes.username.length > 0 &&
+            attributes.newPassword.length > 0 &&
+            attributes.username.indexOf(attributes.newPassword) >= 0
+        ) {
+            errors.push({
+                'message': 'Password can\'t be part of username.',
+                'type': 'newPassword'
+            });
+        }
+
+        if (attributes.newPassword !== attributes.passwordCheck) {
+            errors.push({
+                'message': 'Passwords do not match.',
+                'type': 'passwordCheck'
+            });
+        }
+
+        if (errors.length) {
+            return errors;
+        }
+    }
 });
