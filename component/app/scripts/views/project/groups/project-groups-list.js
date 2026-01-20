c7930d9b5358ba413e0e66f9fbf9f5c2614abfe3
Sam Wollenburg <Samuel.Wollenburg@UTSouthwestern.edu>
2026-01-19 20:27:19 -0600

    fix missing "add rearrangment filter" action when switching filter type

--- component/app/scripts/views/project/groups/project-groups-list.js
+++ component/app/scripts/views/project/groups/project-groups-list.js
@@ -353,6 +353,16 @@ var GroupsDetailView = Marionette.View.extend({
 
                 // remove filter
                 this.model.updateRepertoireFilter(null);
+
+                if (this.model.rearrangement_mode) {
+                    doc.find('.repertoire-groups-row-rearrangement').attr('hidden', false);
+                    doc.find('#project-repertoire-group-add-rearrangement-filter').attr('disabled', true);
+                    doc.find('#project-repertoire-group-delete-rearrangement-filter').attr('disabled', false);
+                } else {
+                    doc.find('.repertoire-groups-row-rearrangement').attr('hidden', true);
+                    doc.find('#project-repertoire-group-add-rearrangement-filter').attr('disabled', false);
+                    doc.find('#project-repertoire-group-delete-rearrangement-filter').attr('disabled', true);
+                }
             }
 
             return;
