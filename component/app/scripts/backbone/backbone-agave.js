9586f16391be048a8d467856dda946be115beb3b
Scott Christley <scott.christley@utsouthwestern.edu>
2026-02-20 19:11:33 -0600

    copy over project uuid

--- component/app/scripts/backbone/backbone-agave.js
+++ component/app/scripts/backbone/backbone-agave.js
@@ -371,6 +371,9 @@ Agave.MetadataModel = Agave.Model.extend({
         let m = this.clone();
         let value = this.get('value');
         m.set('value', JSON.parse(JSON.stringify(value)));
+        if (this.projectUuid) {
+            m.projectUuid = this.projectUuid;
+        }
         return m;
     },
 
