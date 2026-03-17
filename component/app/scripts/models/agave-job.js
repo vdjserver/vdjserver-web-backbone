8b69cb1eed46432c786470148d1da827532ab4ec
Sam Wollenburg <Samuel.Wollenburg@UTSouthwestern.edu>
2026-03-17 14:55:58 -0500

    fix getUniqueTagsForTool; remove tag whitespace when splitting

--- component/app/scripts/models/agave-job.js
+++ component/app/scripts/models/agave-job.js
@@ -792,9 +792,9 @@ export var AnalysisDocument = Agave.MetadataModel.extend({
         for (let entityID in this.provenance[tool]['data']['value']['entity']) {
             let entity = this.provenance[tool]['data']['value']['entity'][entityID];
             if (entity['vdjserver:tags']) {
-                let fields = entity['vdjserver:tags'].split(', ');
+                let fields = entity['vdjserver:tags'].split(',');
                 for (let field of fields) {
-                    tagList.add(field);
+                    tagList.add(field.replace(/\s/g,''));
                 }
             }
         }
